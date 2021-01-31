08a6cb93a46a53ccb6ea5f1dd848135ff9232daf
radulfr-dev <cameron.raw89@gmail.com>
2021-01-31 11:35:39 +0100

    Redux ha estado configurado correctamente

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={ store }>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/reducers/testReducer.js
@@ -0,0 +1,10 @@
+const testReducer = (state = "Hi", action) => {
+  switch (action.type) {
+    case "TEST":
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default testReducer;
