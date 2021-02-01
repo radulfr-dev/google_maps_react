772040cd7d2321bf0f0825bd25ff395436c14ea6
radulfr-dev <cameron.raw89@gmail.com>
2021-02-02 00:55:42 +0100

    Recibido longitude y latitude de busquedas con exito del api geocoding de google

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -8,10 +8,56 @@ function SearchBox() {
   const address = useSelector(state => state.address);
   const dispatch = useDispatch();
 
-  console.log(address);
+  const handleSelect = address => {
+      geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };  
+
 
   return (
         <div className="SearchBox">
+                  <PlacesAutocomplete
+        value={address}
+        onChange={(e) => {
+            dispatch(updateAddress(e));
+        }}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div key={suggestion.index}
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
             <form>
                 <input onChange={ (e) => {
                     dispatch(updateAddress(e.target.value))
@@ -24,7 +70,7 @@ function SearchBox() {
 }
 
 const mapStateToProps = state => ({
-    address: state.address.test
+    address: state.address
 });
 
 export default connect(mapStateToProps, {updateAddress})(SearchBox);
